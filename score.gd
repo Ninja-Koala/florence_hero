81c50225bce9259c359fbf3e3f3f017e37c71a86
Ninja-Koala <mail@ninjakoa.la>
2017-11-04 21:54:23 +0100

    Make Score functional

---
+++ score.gd
@@ -0,0 +1,14 @@
+extends Label
+
+onready var song_seq = get_node("../SongSequencer")
+
+var cur_score = 0
+
+func _ready():
+	song_seq.connect("score_points", self, "update_score")
+	set_text("Score:"+str(cur_score))
+	
+func update_score(score_val):
+	print(score_val)
+	cur_score += score_val
+	set_text("Score:"+str(cur_score))
