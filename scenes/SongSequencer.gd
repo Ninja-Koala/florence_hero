3106e59be97dfeffe258d7c60e0fbcb80d7cb53c
Hendrik Roth <hendrik.roth@freiblick.co>
2017-11-04 13:31:55 +0100

    blaaa

--- scenes/SongSequencer.gd
+++ scenes/SongSequencer.gd
@@ -14,6 +14,9 @@ func initialize(player, song):
 	self.player = player
 	self.song = song
 
+func set_button_state(var button, var new_state):
+	
+
 func advance(ms):
 	#calculate position before and after the advancement
 	var tick_before = current_tick()
@@ -32,8 +35,11 @@ func advance(ms):
 	#play the tick
 	song.play_notes_at(player, tick)
 
-func get_current_noted(var count):
+func get_current_notes(var count):
 	return song.get_notes_in_range(current_tick(), count)
+
+func get_current_offset():
+	return float(ms_per_tick) / (current_ms % ms_per_tick)
 
 func current_tick():
 	return current_ms / ms_per_tick
@@ -41,3 +47,10 @@ func current_tick():
 func get_score(var buttons):
 	var notes = song.get_notes_in_range(current_tick())[0]
 	return notes == buttons
+
+enum ButtonInputType {
+	None,
+	Pressed,
+	Held,
+	Released
+}
