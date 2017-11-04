55d00ae40553008f20829ea78db4c3f09535f681
Ninja-Koala <mail@ninjakoa.la>
2017-11-05 00:28:06 +0100

    Add key hints

--- score.gd
+++ score.gd
@@ -6,9 +6,9 @@ var cur_score = 0
 
 func _ready():
 	song_seq.connect("score_points", self, "update_score")
-	set_text("Score:"+str(cur_score))
+	set_text("Score: "+str(cur_score))
 	
 func update_score(score_val):
 	print(score_val)
 	cur_score += score_val
-	set_text("Score:"+str(cur_score))
+	set_text("Score: "+str(cur_score))
