90804d2c3cd5887cc88eee3927d0b73c9293151b
Simon Budig <simon@budig.de>
2017-11-04 15:18:21 +0100

    Keys 1-3 change aspects of the face

--- scenes/Florence Foster Jennings.gd
+++ scenes/Florence Foster Jennings.gd
@@ -3,12 +3,15 @@ extends Node2D
 onready var eyelids = $Gesicht/Augenlider
 onready var chin = $Gesicht/Unterlippe
 onready var upper_lip = $Gesicht/Oberlippe
+onready var cheeks = $"Gesicht/Bäckchen"
 
 var chin_transform
 var upper_lip_transform
 
 var eyes_closed = false
 var eye_blink_time = 0
+
+var mouth_closed = false
 
 func _ready():
 	chin_transform = chin.transform
@@ -23,11 +26,43 @@ func eyes_open (state):
 		eyelids.show ()
 		eyes_closed = true
 
-func open_mouth (percent):
-	chin.transform = chin_transform * Transform2D (0, Vector2 (12,24) * percent)
-	if percent > 0.7:
-		upper_lip.transform = upper_lip_transform * Transform2D (0, Vector2 (-2,-8) * (percent - 0.7) / 0.3)
 
+func open_mouth (percent):
+	if mouth_closed:
+		chin.transform = chin_transform
+		upper_lip.transform = upper_lip_transform
+	else:
+		chin.transform = chin_transform * Transform2D (0, Vector2 (12,24) * percent)
+		if percent > 0.7:
+			upper_lip.transform = upper_lip_transform * Transform2D (0, Vector2 (-2,-8) * (percent - 0.7) / 0.3)
+		else:
+			upper_lip.transform = upper_lip_transform
+
+
+func smile (state):
+	if state:
+		eyes_open (false)
+		open_mouth (0)
+		mouth_closed = true
+		cheeks.show ()
+	else:
+		eyes_open (true)
+		mouth_closed = false
+		cheeks.hide ()
+	
+
+func _input (event):
+	if event.is_action_pressed ("face_smile"):
+		smile (true)
+	elif event.is_action_released ("face_smile"):
+		smile (false)
+	
+	if event.is_action_pressed ("face_eyes_open"):
+		eyes_open (true)
+	
+	if event.is_action_pressed ("face_eyes_close"):
+		eyes_open (false)
+		
 	
 func _process (delta):
 	eye_blink_time += delta
