5dd4ef6df96fab9d62373e7b6bd795d053897398
Simon Budig <simon@budig.de>
2017-11-04 14:54:46 +0100

    improved animation

--- scenes/Florence Foster Jennings.gd
+++ scenes/Florence Foster Jennings.gd
@@ -1,15 +1,43 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+onready var eyelids = $Gesicht/Augenlider
+onready var chin = $Gesicht/Unterlippe
+onready var upper_lip = $Gesicht/Oberlippe
+
+var chin_transform
+var upper_lip_transform
+
+var eyes_closed = false
+var eye_blink_time = 0
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	chin_transform = chin.transform
+	upper_lip_transform = upper_lip.transform
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func eyes_open (state):
+	if state:
+		eyelids.hide ()
+		eyes_closed = false
+		eye_blink_time = 0
+	else:
+		eyelids.show ()
+		eyes_closed = true
+
+func open_mouth (percent):
+	chin.transform = chin_transform * Transform2D (0, Vector2 (12,24) * percent)
+	if percent > 0.7:
+		upper_lip.transform = upper_lip_transform * Transform2D (0, Vector2 (-2,-8) * (percent - 0.7) / 0.3)
+
+	
+func _process (delta):
+	eye_blink_time += delta
+	if eye_blink_time > 4:
+		eye_blink_time = 0
+		
+	if eye_blink_time < 3.9 and not eyes_closed:
+		eyelids.hide ()
+	else:
+		eyelids.show ()
+		
+	open_mouth ((1 + sin (3 * (eye_blink_time / 2.0 * PI))) / 2)
+
