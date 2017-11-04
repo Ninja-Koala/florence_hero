5b9621fd5f7e0900a07e829e598a6027968c4a43
Ninja-Koala <mail@ninjakoa.la>
2017-11-04 16:23:03 +0100

    Foo

--- scenes/music_bar.gd
+++ scenes/music_bar.gd
@@ -4,12 +4,15 @@ onready var song_seq = $SongSequencer
 onready var note_player = $NotePlayer
 onready var demo_song_resource = load("res://songs/DemoSong.gd")
 onready var song = load("res://classes/Song.gd")
-onready var note_bar = load("res://scenes/note.tscn")
+onready var single_bar = load("res://scenes/note.tscn")
+onready var bar_begin = load("res://scenes/note_begin.tscn")
+onready var bar_held = load("res://scenes/note_held.tscn")
+onready var bar_end = load("res://scenes/note_end.tscn")
 
 var note_bars = []
 var note_bar_pos = []
 
-var base_pos = Vector2(-280,-150)
+var base_pos = Vector2(-360,-185)
 var x_offset = Vector2(192,0)
 var y_offset = Vector2(0,102)
 
@@ -34,7 +37,28 @@ func _process(delta):
 		for i in range(4):
 			for j in range(4):
 					if note_array[i][j] == song.NoteType.Single:
-						var bar = note_bar.instance()
+						var bar = single_bar.instance()
+						add_child(bar)
+						var pos = base_pos + i*x_offset + (3-j) * y_offset
+						bar.set_position(pos)
+						note_bars += [bar]
+						note_bar_pos += [pos]
+					elif note_array[i][j] == song.NoteType.Pressed:
+						var bar = bar_begin.instance()
+						add_child(bar)
+						var pos = base_pos + i*x_offset + (3-j) * y_offset
+						bar.set_position(pos)
+						note_bars += [bar]
+						note_bar_pos += [pos]
+					elif note_array[i][j] == song.NoteType.Held:
+						var bar = bar_held.instance()
+						add_child(bar)
+						var pos = base_pos + i*x_offset + (3-j) * y_offset
+						bar.set_position(pos)
+						note_bars += [bar]
+						note_bar_pos += [pos]
+					elif note_array[i][j] == song.NoteType.Released:
+						var bar = bar_end.instance()
 						add_child(bar)
 						var pos = base_pos + i*x_offset + (3-j) * y_offset
 						bar.set_position(pos)
