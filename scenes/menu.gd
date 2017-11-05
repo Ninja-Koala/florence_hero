b152e13b8c00e6f99d6530130a7642dbb4eb0735
Simon Budig <simon@budig.de>
2017-11-05 13:29:58 +0100

    mozart song

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -12,5 +12,5 @@ func _on_Button1_button_down():
 	
 
 func _on_Button2_button_down():
-	song_selector.song_path = "res://songs/DemoSong1.csv"
+	song_selector.song_path = "res://songs/mozart_queen.csv"
 	get_tree().change_scene_to(main_scene)
