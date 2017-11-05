8188d37802bd51c08adc7de8d8b136b13fec614d
Hendrik Roth <hendrik.roth@freiblick.co>
2017-11-05 14:26:46 +0100

    exit button and fullscreen

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -6,6 +6,10 @@ onready var song_selector = get_node("/root/song_selector")
 func _ready():
 	pass
 
+func _input(event):
+	if event.is_action_pressed ("fullscreen"):
+		OS.set_window_fullscreen (not OS.is_window_fullscreen ())
+
 func _on_Button1_button_down():
 	song_selector.song_path = "res://songs/DemoSong1.txt"
 	get_tree().change_scene_to(main_scene)
@@ -14,3 +18,7 @@ func _on_Button1_button_down():
 func _on_Button2_button_down():
 	song_selector.song_path = "res://songs/mozart_queen.txt"
 	get_tree().change_scene_to(main_scene)
+
+
+func on_exit_button_pressed():
+	get_tree().quit()	
